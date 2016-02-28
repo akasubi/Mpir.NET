2f298e87170ac166f9c7f9e4827f20f6f92ce93c
jr <john@wezeku.com>
2016-02-28 19:02:35 +0100

    Update generated documentation for version 0.3.3

---
+++ doxygenOutput/html/search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghijklmnoprstvxz",
+  1: "gm",
+  2: "ms",
+  3: "adgmx",
+  4: "abcdefghijklmnoprstx",
+  5: "notvz",
+  6: "gm",
+  7: "bt"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "typedefs",
+  7: "properties"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Variables",
+  6: "Typedefs",
+  7: "Properties"
+};
+
